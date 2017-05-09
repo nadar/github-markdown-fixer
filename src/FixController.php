5c21f8ca907ec2be94fb4e73174fb2f85df1fe80
Basil Suter <git@nadar.io>
2017-05-09 10:14:48 +0200

    find unicode

--- src/FixController.php
+++ src/FixController.php
@@ -19,29 +19,12 @@ class FixController extends Command
 
         foreach ($files as $file) {
             $content = file_get_contents($file);
-            
-            $content = str_replace("\xA0", " ", $content);
-            
-            file_put_contents($file, $content);
+            file_put_contents($file, $this->parseContent($content));
         }
     }
-    
-    private function unichr($c)
+
+    private function parseContent($content)
     {
-        if (ord($c{0}) >=0 && ord($c{0}) <= 127)
-            return ord($c{0});
-            if (ord($c{0}) >= 192 && ord($c{0}) <= 223)
-                return (ord($c{0})-192)*64 + (ord($c{1})-128);
-                if (ord($c{0}) >= 224 && ord($c{0}) <= 239)
-                    return (ord($c{0})-224)*4096 + (ord($c{1})-128)*64 + (ord($c{2})-128);
-                    if (ord($c{0}) >= 240 && ord($c{0}) <= 247)
-                        return (ord($c{0})-240)*262144 + (ord($c{1})-128)*4096 + (ord($c{2})-128)*64 + (ord($c{3})-128);
-                        if (ord($c{0}) >= 248 && ord($c{0}) <= 251)
-                            return (ord($c{0})-248)*16777216 + (ord($c{1})-128)*262144 + (ord($c{2})-128)*4096 + (ord($c{3})-128)*64 + (ord($c{4})-128);
-                            if (ord($c{0}) >= 252 && ord($c{0}) <= 253)
-                                return (ord($c{0})-252)*1073741824 + (ord($c{1})-128)*16777216 + (ord($c{2})-128)*262144 + (ord($c{3})-128)*4096 + (ord($c{4})-128)*64 + (ord($c{5})-128);
-                                if (ord($c{0}) >= 254 && ord($c{0}) <= 255)    //  error
-                                    return FALSE;
-                                    return 0;
+        return preg_replace('/xC2xA0/',' ',$content);
     }
 }
